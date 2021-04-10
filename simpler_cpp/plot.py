1e9a3be81374aad2bccf73668a6d33d70b3ed9b2
Xingchang Huang <xhuang@mpi-inf.mpg.de>
2021-04-10 22:16:08 +0200

    try running on cluster

--- simpler_cpp/plot.py
+++ simpler_cpp/plot.py
@@ -138,11 +138,11 @@ def plot_txt(scene_name):
 
 def main():
     # read_all_txt()
-    # plot_txt('praise_the_sun')
-    # plot_txt('zerg_rush')
+    plot_txt('praise_the_sun')
+    plot_txt('zerg_rush')
     plot_txt('forest')
-   # plot_txt('constrained')
-    # plot_txt('constrained_overlap')
+    plot_txt('constrained')
+    plot_txt('constrained_overlap')
 
 
 if __name__ == '__main__':
--- simpler_cpp/scripts/forest.sh
+++ simpler_cpp/scripts/forest.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#SBATCH -p gpu20
+#SBATCH -p cpu20
 #SBATCH -t 24:00:00
 #SBATCH -o slurm-%j.out
-#SBATCH --gres gpu:1
 
 # run in build folder
 ./DiskProject ../configs/forest.txt
